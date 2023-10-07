722f48ffec1e3605a574ca8cebcf7e4b299c6ee7
IT <italo@kampa.io>
2023-10-06 22:17:01 -0300

    add script in repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Update you tux 
+
+script created to run the main commands to update your debian-based linux distribution
---
+++ update-tux.sh
@@ -0,0 +1,15 @@
+"#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+echo -e "\e[01;37;45m========== ATUALIZADOR DE PROGRAMAS MELHOR QUE A POP SHOP ==========\e[0m"
+echo -e "Autor: Italo Nogueira  / \nAno: 2023"
+echo -e "\e[37;45m====================================================================\e[0m"
+echo -e "Olá Usuário, bem vindo ao utilitário de atualização.\nEsse script vai atualizar seus arquivos .deb, flatpaks e snaps \nO programa fechará automáticamente quando as atualizações forem concluídas. \nDigite sua senha para começar"
+#.deb
+sudo apt update
+sudo apt full-upgrade
+sudo apt dist-upgrade
+#flatpak
+flatpak update
+#snap
+snap refresh"
+
