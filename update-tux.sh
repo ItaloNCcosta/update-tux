62df7ac7e79cea707cc3c201d0d05e16b9e9cf7f
IT <italo@kampa.io>
2024-05-25 11:38:10 -0300

    fix: update script

--- update-tux.sh
+++ update-tux.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 
 # -*- ENCODING: UTF-8 -*-
-echo -e "\e[01;37;45m========== ATUALIZADOR DE PROGRAMAS ==========\e[0m"
-echo -e "Autor: Italo Nogueira  / \nAno: 2023"
-echo -e "\e[37;45m====================================================================\e[0m"
-echo -e "Olá Usuário, bem vindo ao utilitário de atualização.\nEsse script vai atualizar seus arquivos .deb, flatpaks e snaps \nO programa fechará automáticamente quando as atualizações forem concluídas. \nDigite sua senha para começar"
+# Atualizador de Programas
+# Autor: Italo Nogueira
+# Ano: 2023
 
-#.deb
-echo "sudo apt update"
-sudo apt update
+printf "\e[01;37;45m========== ATUALIZADOR DE PROGRAMAS ==========\e[0m\n"
+printf "Autor: Italo Nogueira  / \nAno: 2023\n"
+printf "\e[37;45m====================================================================\e[0m\n"
+printf "Olá Usuário, bem-vindo ao utilitário de atualização.\n"
+printf "Esse script vai atualizar seus arquivos .deb, flatpaks e snaps.\n"
+printf "O programa fechará automaticamente quando as atualizações forem concluídas.\n"
+printf "Digite sua senha para começar\n"
 
-echo "sudo apt full-upgrade"
-sudo apt full-upgrade
+# Atualização de pacotes .deb
+printf "\nAtualizando pacotes .deb...\n"
+sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean
+if [ $? -eq 0 ]; then
+  printf "Pacotes .deb atualizados com sucesso.\n"
+else
+  printf "\e[31mFalha na atualização dos pacotes .deb.\e[0m\n"
+  exit 1
+fi
 
-echo "sudo apt dist-upgrade"
-sudo apt dist-upgrade
+# Atualização de pacotes flatpak
+printf "\nAtualizando pacotes flatpak...\n"
+flatpak update -y
+if [ $? -eq 0 ]; then
+  printf "Pacotes flatpak atualizados com sucesso.\n"
+else
+  printf "\e[31mFalha na atualização dos pacotes flatpak.\e[0m\n"
+  exit 1
+fi
 
-#flatpak
-echo "flatpak update"
-flatpak update
+# Atualização de pacotes snap
+printf "\nAtualizando pacotes snap...\n"
+sudo snap refresh
+if [ $? -eq 0 ]; then
+  printf "Pacotes snap atualizados com sucesso.\n"
+else
+  printf "\e[31mFalha na atualização dos pacotes snap.\e[0m\n"
+  exit 1
+fi
 
-#snap
-echo "snap refresh"
-snap refresh
-
+printf "\e[32m\nTodas as atualizações foram concluídas com sucesso.\e[0m\n"
+exit 0
